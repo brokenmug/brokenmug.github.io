2298201642537720572216a3f686ca870e69f375
igorfel <appsigor@gmail.com>
2018-11-17 04:10:55 -0200

    Changed divs to sections, added Newsletter and fonts

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.subscribe').click(function () {
+        if($(this).hasClass('btn-primary')) {
+            $('.form-email').addClass('hidden');
+            $(this).removeClass('btn-primary');
+
+            $(this).html('Subscribed <i class="fas fa-check"></i>');
+            $(this).addClass('btn-success');
+            $(this).parent().parent().append('<span style="color: #555">Just kidding, this is just a test :D</span>');
+        }
+    });
+});
